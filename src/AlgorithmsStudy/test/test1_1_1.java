114c162f59b57069efaed1046c066317f9b50599
Larry Han <hanlin824@sina.com>
2021-01-16 23:31:57 +0800

    some code

--- src/AlgorithmsStudy/test/test1_1_1.java
+++ src/AlgorithmsStudy/test/test1_1_1.java
@@ -9,8 +9,16 @@ public class test1_1_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double a = 2.0e-6 * 100000000.1;
-		
+		System.out.println(a);
 		StdOut.print(1+2+"3");
+		
+		test testobj = new test();
+		testobj.testfun();
 	}
 
+}
+class test{
+	public static void testfun(){
+		System.out.println("testfun");
+	}
 }
